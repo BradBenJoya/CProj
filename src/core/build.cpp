a397970750603e474dfefedf0fb6447ed9935953
BradBenJoya <bradbenjoya6@gmail.com>
2026-08-19 11:01:30 +0300

    Replace shell commands with Boost.Process

--- src/core/build.cpp
+++ src/core/build.cpp
@@ -1,10 +1,13 @@
 #include "build.h"
+#include <boost/process/v1/child.hpp>
+#include <boost/process/v1/search_path.hpp>
 #include <print>
 #include <iostream>
 #include <format>
 
 namespace cproj_core {
 	void handleBuild(std::filesystem::path path) {
+		namespace process = boost::process::v1;
 		if (!std::filesystem::exists(path / "CMakeLists.txt")) {
 			std::println(std::cerr, "Could not find CMakeLists.txt in provided path {}", path.string());
 			return;
@@ -12,14 +15,18 @@ namespace cproj_core {
 
 		if (!std::filesystem::is_empty(path)) {
 			std::println("Configuring with CMake...");
-			if (std::system(std::format("cmake -S \"{}\" -B \"{}/bin\" -G Ninja", path.string(), path.string()).c_str()) != 0) {
+			process::child cmake(process::search_path("cmake"), "-S", path.string(), "-B", (path / "bin").string(), "-G", "Ninja");
+			cmake.wait();
+			if (cmake.exit_code() != 0) {
 				std::println(std::cerr, "Failed to configure with CMake");
 				return;
 			}
 		}
 
 		std::println("Building project...");
-		if (std::system(std::format("cmake --build \"{}/bin\"", path.string()).c_str()) != 0) {
+		process::child cmake(process::search_path("cmake"), "--build", (path / "bin").string());
+		cmake.wait();
+		if (cmake.exit_code() != 0) {
 			std::println(std::cerr, "Failed to build project");
 			return;
 		}
